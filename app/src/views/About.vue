fddc2c101c8d461993dd3ae2ab85515d45824bfb
dtkelley <dtkelley@calpoly.edu>
2018-10-22 17:11:43 -0700

    Story #10 - Basic look for store hours

--- app/src/views/About.vue
+++ app/src/views/About.vue
@@ -1,5 +1,70 @@
 <template>
   <div class="about">
-    <h1>This is the about page</h1>
+    <div id="location-hours">
+      <div id="hours-header">
+        <h1>Foxcycle San Luis Obispo</h1>
+        <body>123 Higuera St</body>
+        <body>San Luis Obispo, CA 93405</body>
+        <h3>Store Hours</h3>
+      </div>
+      <table id="hours">
+        <tr>
+          <td>Monday</td>
+          <td>10:00 AM</td>
+          <td>8:00 PM</td>
+        </tr>
+        <tr>
+          <td>Tuesday</td>
+          <td>10:00 AM</td>
+          <td>8:00 PM</td>
+        </tr>
+        <tr>
+          <td>Wednesday</td>
+          <td>10:00 AM</td>
+          <td>8:00 PM</td>
+        </tr>
+        <tr>
+          <td>Thursday</td>
+          <td>10:00 AM</td>
+          <td>8:00 PM</td>
+        </tr>
+        <tr>
+          <td>Friday</td>
+          <td>10:00 AM</td>
+          <td>8:00 PM</td>
+        </tr>
+        <tr>
+          <td>Saturday</td>
+          <td>10:00 AM</td>
+          <td>6:00 PM</td>
+        </tr>
+        <tr>
+          <td>Sunday</td>
+          <td>10:00 AM</td>
+          <td>6:00 PM</td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
+
+<style>
+  .about {
+    padding: 20px;
+  }
+
+  #location-hours {
+    border: 1px solid #1B2222;
+    text-align: left;
+    padding: 10px;
+    width: 30%;
+  }
+
+  #hours {
+    width: 100%;
+  }
+
+  tr:nth-child(odd)  {
+    background-color: #f2f2f2; 
+  }
+</style>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -2,6 +2,51 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div id="location-hours">
+      <div id="hours-header">
+        <h1>Foxcycle San Luis Obispo</h1>
+        <body>123 Higuera St</body>
+        <body>San Luis Obispo, CA 93405</body>
+        <h3>Store Hours</h3>
+      </div>
+      <table id="hours">
+        <tr>
+          <td>Monday</td>
+          <td>10:00 AM</td>
+          <td>8:00 PM</td>
+        </tr>
+        <tr>
+          <td>Tuesday</td>
+          <td>10:00 AM</td>
+          <td>8:00 PM</td>
+        </tr>
+        <tr>
+          <td>Wednesday</td>
+          <td>10:00 AM</td>
+          <td>8:00 PM</td>
+        </tr>
+        <tr>
+          <td>Thursday</td>
+          <td>10:00 AM</td>
+          <td>8:00 PM</td>
+        </tr>
+        <tr>
+          <td>Friday</td>
+          <td>10:00 AM</td>
+          <td>8:00 PM</td>
+        </tr>
+        <tr>
+          <td>Saturday</td>
+          <td>10:00 AM</td>
+          <td>6:00 PM</td>
+        </tr>
+        <tr>
+          <td>Sunday</td>
+          <td>10:00 AM</td>
+          <td>6:00 PM</td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -16,3 +61,21 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
   })
 export default class Home extends Vue {}
 </script>
+
+<style>
+  #location-hours {
+    border: 1px solid #1B2222;
+    text-align: left;
+    padding: 10px;
+    width: 30%;
+  }
+
+  #hours {
+    width: 100%;
+  }
+
+  tr:nth-child(odd)  {
+    background-color: #f2f2f2; 
+  }
+</style>
+
