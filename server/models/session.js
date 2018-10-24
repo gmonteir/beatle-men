fb99975341f06908f4c680af1ba3e34a5b54cca5
Ilyam123 <ilya12508@gmail.com>
2018-10-23 20:09:37 -0700

    added session.js and changed gitignore

---
+++ server/models/session.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Session = sequelize.define('Session', {
+    expiration: DataTypes.DATE
+  }, {});
+  Session.associate = function(models) {
+    // associations can be defined here
+    Session.belongsTo(models.User);
+  };
+  return Session;
+};
