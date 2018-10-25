d4e65725d37624bf25442d21259e0ada63161367
Andy Khov <khovandyak@gmail.com>
2018-10-24 21:54:06 -0700

    stories #35 #36, finished login and signup front end

--- app/src/store.ts
+++ app/src/store.ts
@@ -5,10 +5,21 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    firstName: null,
+    lastName: null,
+    email: null,
   },
   mutations: {
-
+    modifyName(state, payload) {
+      // eslint-disable-next-line
+      state.firstName = payload.firstName;
+      // eslint-disable-next-line
+      state.lastName = payload.lastName;
+    },
+    modifyEmail(state, payload) {
+      // eslint-disable-next-line
+      state.email = payload.email;
+    },
   },
   actions: {
 
--- app/src/views/Login.vue
+++ app/src/views/Login.vue
@@ -6,35 +6,94 @@
     <div class="field">
       <label class="label">Email</label>
       <div class="control">
-        <input class="input" type="text" placeholder="name@email"/>
+        <input class="input"
+          type="email"
+          placeholder="name@domain"
+          v-model="email"
+          v-bind:class="{'is-danger': isEmailInvalid}"
+        />
+        <p class="help is-danger" v-if="isEmailInvalid">Invalid Email</p>
       </div>
     </div>
     <div class="field">
       <label class="label">Password</label>
       <div class="control">
-        <input class="input" type="text" placeholder="************"/>
+        <input class="input"
+          type="password"
+          placeholder="******"
+          v-model="password"
+          v-bind:class="{'is-danger': isPasswordInvalid}"
+        />
+        <p class="help is-danger"
+          v-if="isPasswordInvalid">
+          Invalid Password
+        </p>
       </div>
     </div>
     <div class="field">
       <div class="control">
+        <div class="content has-text-success is-pulled-left"
+          v-if="isLoginSuccess">Login Successful!
+        </div>
         <div class="buttons is-right">
-          <button class="button is-link">
-            <strong>Submit</strong>
-          </button>
+          <button class="button" v-on:click="close">Close</button>
+          <button class="button is-link" v-on:click="submit">Submit</button>
         </div>
       </div>
     </div>
   </div>
-  <button class="modal-close is-large" aria-label="close" v-on:click="close"></button>
 </div>
 </template>
 
 <script>
+/* true if invalid, false if valid */
+function validateEmail(email) {
+  if (email === null) {
+    return true;
+  }
+  /* use a regular expression to check if 'email' is valid */
+  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return !regex.test(String(email).toLowerCase());
+}
+
+/* true if invalid, false if valid */
+function validatePassword(password) {
+  if (password === null) {
+    return true;
+  }
+  return password.length === 0;
+}
+
 export default {
   name: 'login',
+  data() {
+    return {
+      email: null,
+      password: null,
+      isEmailInvalid: null,
+      isPasswordInvalid: null,
+      isLoginSuccess: null,
+    };
+  },
   methods: {
+    /* function is called when the 'close' button is clicked */
     close() {
       this.$router.go(-1); /* router goes back one step in user history */
+    },
+    /* function is called when the 'submit' button is clicked */
+    submit() {
+      this.isEmailInvalid = validateEmail(this.email);
+      this.isPasswordInvalid = validatePassword(this.password);
+      this.isLoginSuccess = !this.isEmailInvalid &&
+        !this.isPasswordInvalid &&
+        (this.email != null) &&
+        (this.password != null);
+      /* change vuex store state with mutations */
+      if (this.isLoginSuccess) {
+        this.$store.commit('modifyEmail', {
+          email: this.email,
+        });
+      }
     },
   },
 };
