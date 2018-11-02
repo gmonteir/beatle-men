e205394c91f204e51a29aef4832c6079d6e2a939
jjohn163 <jjohn163@calpoly.edu>
2018-11-01 18:29:59 -0700

    doesn't create account if already exists

--- server/routes/useraccounts.js
+++ server/routes/useraccounts.js
@@ -4,20 +4,30 @@ const { UserAccount } = require('../models');
 const router = express.Router();
 
 router
-  .route('/').post((req, res) => {
+  .route('/')
+  .post((req, res) => {
     const { firstName,lastName,email,username,password,address,accountType } = req.body;
-    const newUserAccount = UserAccount.build({
-      firstName,
-      lastName,
-      email,
-      username,
-      password,
-      address,
-      accountType
-    });
-    newUserAccount.save().then(() => {
-      res.json(newUserAccount);
-    });
+    const user = UserAccount.findOne({where: { email } }).then(user => {
+      if(user){
+        res.status(201).json({error: "account already exists"})
+      }
+      else{
+        const newUserAccount = UserAccount.build({
+          firstName,
+          lastName,
+          email,
+          username,
+          password,
+          address,
+          accountType
+        });
+        newUserAccount.save().then(() => {
+          res.json(newUserAccount);
+        });
+      }
+    })
+
   });
+
 
 module.exports = router;
